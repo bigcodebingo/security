17bda6b79e288e4f22d9bc7ac6046966a0d45ec7
gach1van <102760471+bigcodebingo@users.noreply.github.com>
2025-09-22 14:58:04 +0300

    change files

---
+++ algorithms/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "permutation.h"
+#include "stierlitz.h"
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int option;
+
+    cout << "1. проверка permutation\n";
+    cout << "2. расшифровка stierlitz\n";
+    cout << "0. выход\n";
+
+    while (true) {
+        
+        cout << "\nваш выбор: ";
+        cin >> option;
+
+        if (option == 1) {
+            runPermutation();
+        }
+        else if (option == 2) {
+            runStierlitz();
+        }
+        else if (option == 0) {
+            cout << "выход из программы..." << endl;
+            break;
+        }
+        else {
+            cout << "неверный выбор" << endl;
+        }
+    }
+}
